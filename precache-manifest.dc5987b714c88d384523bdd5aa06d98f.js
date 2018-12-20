a3c60e639e64e6cfb87fcc72120afd0c958eac91
sarfarazkasmani <sarfarazkasmani@gmail.com>
2018-12-20 12:52:30 +0530

    Updates

---
+++ precache-manifest.dc5987b714c88d384523bdd5aa06d98f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "8970f709455cd494c0ed",
+    "url": "/todolist/static/js/main.8970f709.chunk.js"
+  },
+  {
+    "revision": "9c376d4822a8f1f54887",
+    "url": "/todolist/static/js/1.9c376d48.chunk.js"
+  },
+  {
+    "revision": "8970f709455cd494c0ed",
+    "url": "/todolist/static/css/main.02e2dab2.chunk.css"
+  },
+  {
+    "revision": "a30ef8ae27ba75c266f92e5149d535fe",
+    "url": "/todolist/index.html"
+  }
+];
