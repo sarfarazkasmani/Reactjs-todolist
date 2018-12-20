4cddc07f865d6e2a6106a7ee7377f236008598e3
sarfarazkasmani <sarfarazkasmani@gmail.com>
2018-12-20 11:57:27 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      userInput: '',
+      list: []
+    }
+    // this.removeTodo = this.removeTodo.bind(this)
+  }
+
+  updateValue(input) {
+    this.setState({
+      userInput: input
+    });
+  }
+
+  addTodoList(e) {
+    e.preventDefault();
+    this.setState({
+      list: [this.state.userInput, ...this.state.list],
+      userInput: ''
+    })
+  }
+
+  // removeTodo(index) {
+  //   let list = this.state.list.slice();
+  //   list.splice(index, 1);
+  //   this.setState({
+  //     list
+  //   });
+  // }
+
   render() {
+    // if (this.state.userInput === '') {
+    //   alert('Please enter task')
+    // } else {
+    //   this.setState({
+    //     list
+    // });
+    // }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={(e) => this.addTodoList(e)}>
+          <input
+            onChange={(e) => this.updateValue(e.target.value)}
+            value={this.state.userInput}
+            type="text"
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {/* <ul>
+          {this.state.list.map((listItem, index) => <li key={index}>{listItem} <button onClick={() => { this.removeTodo(listItem, index)}}>Delete</button></li>)}
+        </ul> */}
+        {/* <ul>
+          {this.state.list.map((listItem, index) => <li key={index}>{listItem} <button onClick={this.removeTodo}>Delete</button></li>)}
+        </ul> */}
+        <ul>
+          {this.state.list.map((listItem, index) => <li key={index}>{listItem} </li>)}
+        </ul>
       </div>
     );
   }
